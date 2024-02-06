6aed2d6f866e655ebe26ec1b055e85465ba9acc9
mjearlb <milobauman@gmail.com>
2024-02-06 08:31:22 -0500

    Added link to homepage

--- demo/info.php
+++ demo/info.php
@@ -11,6 +11,9 @@
     Password is: 
     <?php echo $_POST["password"]; ?>
 
+    <br>
+    <a href="index.html">To Homepage</a>
+    <br>
     <hr>
     <center>Copyright @ 2024</center>
     </body>
